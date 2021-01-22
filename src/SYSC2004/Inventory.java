9862c94c56ca2a29876341e097f07b1c7d71d1e5
JeremyTrendoff81 <69067109+JeremyTrendoff81@users.noreply.github.com>
2021-01-22 16:19:37 -0500

    ola

--- src/SYSC2004/Inventory.java
+++ src/SYSC2004/Inventory.java
@@ -1,4 +1,8 @@
 package SYSC2004;
 
+/**
+ * Authors: Jeremy Trendoff - 101160306, Evan Smedley - 101148695
+ */
+
 public class Inventory {
 }
--- src/SYSC2004/Main.java
+++ src/SYSC2004/Main.java
@@ -1,5 +1,9 @@
 package SYSC2004;
 
+/**
+ * Authors: Jeremy Trendoff - 101160306, Evan Smedley - 101148695
+ */
+
 public class Main {
 
     public static void main(String[] args) {
