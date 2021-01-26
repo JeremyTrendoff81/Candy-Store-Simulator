b6228d2c4775006115b4ce3188536f6dd805c94e
Evan Smedley <evan.smedley22@gmail.com>
2021-01-26 15:37:11 -0500

    Yay

--- src/SYSC2004/StoreManager.java
+++ src/SYSC2004/StoreManager.java
@@ -1,8 +1,47 @@
 package SYSC2004;
 
+import java.util.ArrayList;
+
 /**
  * Authors: Jeremy Trendoff - 101160306, Evan Smedley - 101148695
+ *
+ * Date of Completion: Jan 26, 2021
+ *
+ * Class Description: The StoreManager manages the Inventory, ShoppingCarts and provides
+ * information to the StoreView class.
  */
 
 public class StoreManager {
+    private Inventory inventory;  // The inventory object that the StoreManager manages
+
+    /* Default constructor. Creates a new Inventory object to manage upon creation. */
+    public StoreManager() {
+        inventory = new Inventory(); // New empty Inventory object
+    }
+
+    /* Constructor to create a StoreManager object and initialize the Inventory object with products. */
+    public StoreManager(ArrayList <Product> stockList) {
+        inventory = new Inventory(stockList);
+    }
+
+    /* Check how much stock of a given Product is in the inventory. */
+    public int checkStock(Product product) {
+        return inventory.getStock(product.getId());
+    }
+
+    /*  */
+    public double processTransaction(ArrayList <int[]> purchases) {
+        double totalPrice = 0.0;
+        double tempPrice;
+        for (int[] item : ArrayList) {
+            tempPrice = inventory.getProductPrice(item[0]);
+            if (tempPrice < 0) {
+                return tempPrice;
+            } else {
+                totalPrice += tempPrice;
+                inventory.removeStock(item[0], item[1]);
+            }
+        }
+        return totalPrice;
+    }
 }
