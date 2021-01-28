b952bff3e5674c6d1144efc2e9535c7622184952
JeremyTrendoff81 <69067109+JeremyTrendoff81@users.noreply.github.com>
2021-01-27 19:30:29 -0500

    StoreManager Test Class

---
+++ src/Tests/StoreManagerTest.java
@@ -0,0 +1,18 @@
+package Tests;
+
+import SYSC2004.StoreManager;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StoreManagerTest {
+    private StoreManager sm;
+
+    @Test
+    void checkStock() {
+    }
+
+    @Test
+    void processTransaction() {
+    }
+}
